fbd1e2c2f40bd3c2453910f69bbf2819fb739f19
François Sevestre <francois.sevestre.35@gmail.com>
2020-02-09 13:02:53 +0100

    FIlling header file

--- tasklist.h
+++ tasklist.h
@@ -2,10 +2,35 @@
 #define TASKLIST_H
 #include "ninjatodolalist.h"
 
+
+
+/**
+ * Object that displays itself and its content in an ordonnated way.
+ *
+ */
 class TaskList : public NinjatodolaList
 {
+protected:
+  std::string attrType;
+
 public:
+  // Constructors
   TaskList();
+  TaskList(std::string selfRepr, NinjatodolaObject *motherList, int positionInMotherList, int Indent);
+  ~TaskList();
+
+  // Accessers
+
+  //Get
+  std::string getType();
+
+  // Methodes
+
+  //representation
+  std::vector<NinjatodolaObject*> listRepr();
+
+  //Special
+  void update();
 };
 
 #endif // TASKLIST_H
